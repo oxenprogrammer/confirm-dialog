7fde3149305688e1db1d7134d7d50fd1d8d1f53d
oxenprogrammer <emmy1000okello@gmail.com>
2021-11-27 13:47:08 +0300

    add others

--- confirmDialog.js
+++ confirmDialog.js
@@ -1,29 +1,32 @@
-const modal = document.createElement('section');
-const modelContent = document.createElement('article');
-const xButton = document.createElement('span');
-const modelText = document.createElement('h2');
-const cancelButton = document.createElement('button');
-const yesButton = document.createElement('button');
+class ConfirmDialog {
+  constructor(question = "Are you sure you want to continue?") {
+    this.question = question;
+    
+    const { body } = document;
+    const modal = document.createElement('section');
+    const modelContent = document.createElement('article');
+    const modelText = document.createElement('h2');
+    const cancelButton = document.createElement('button');
+    const yesButton = document.createElement('button');
 
-modal.setAttribute('class', 'model');
-modelContent.setAttribute('class', 'model-content');
-xButton.setAttribute('class', 'close-button');
-cancelButton.setAttribute('class', 'cancel');
-yesButton.setAttribute('class', 'yes');
+    modal.setAttribute('class', 'modal');
+    modelContent.setAttribute('class', 'modal-content');
+    cancelButton.setAttribute('class', 'cancel');
+    yesButton.setAttribute('class', 'yes');
 
-xButton.textContent = `X`;
-cancelButton.textContent = "Cancel";
-yesButton.textContent = "Yes";
-modelText.textContent = "I come from the Main Body";
+    cancelButton.textContent = "Cancel";
+    yesButton.textContent = "Yes";
+    modelText.textContent = this.question;
 
-modelContent.appendChild(xButton);
-modelContent.appendChild(modelText);
-modelContent.appendChild(cancelButton);
-modelContent.appendChild(yesButton);
+    modelContent.appendChild(modelText);
+    modelContent.appendChild(cancelButton);
+    modelContent.appendChild(yesButton);
 
-modal.appendChild(modelContent);
+    modal.appendChild(modelContent);
+    body.appendChild(modal);
+  }
+  
+}
 
-export default modal;
-
-
+export default ConfirmDialog;
 
--- index.js
+++ index.js
@@ -1,4 +1,42 @@
-import modal from "./confirmDialog.js";
-const { body } = document;
+import ConfirmDialog from "./confirmDialog.js";
 
-body.appendChild(modal);
+// Here you can pass to the parent any div content
+new ConfirmDialog("Coming from main");
+
+
+const { body } = document;
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector(".trigger");
+const cancelButton = document.querySelector(".cancel");
+const yesButton = document.querySelector(".yes");
+
+function toggleModal() {
+  modal.classList.toggle("show-modal");
+  const question  = document.querySelector('.para');
+  if(question) {
+    question.innerHTML = "";
+  }
+}
+
+function windowOnClick(event) {
+  if (event.target === modal) {
+      toggleModal();
+  }
+}
+
+trigger.addEventListener("click", toggleModal);
+cancelButton.addEventListener("click", () => {
+  toggleModal();
+  const cancelled = document.createElement('p');
+  cancelled.setAttribute('class', 'para');
+  cancelled.textContent = "You just clicked Cancel";
+  body.appendChild(cancelled);
+})
+yesButton.addEventListener("click", () => {
+  toggleModal();
+  const yes = document.createElement('p');
+  yes.setAttribute('class', 'para');
+  yes.textContent = "You just Clicked Yes";
+  body.appendChild(yes);
+})
+window.addEventListener("click", windowOnClick);
